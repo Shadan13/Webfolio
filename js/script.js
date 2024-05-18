bd743bac873fede09f1ee55b14cfcff1d90fae72
Shadan Siddiqui <shadan.siddiqui@gmail.com>
2024-05-18 22:11:59 +0800

    Add scroll to top button

--- js/script.js
+++ js/script.js
@@ -15,3 +15,23 @@ window.addEventListener('scroll', () => {
 
     prevScrollPos = currentScrollPos;
 });
+
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+    });
+}
+
+let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
+
+window.addEventListener('scroll', function () {
+    var scrollTopButton = document.querySelector('.scroll-top');
+    if (this.window.scrollY > 200) {
+        scrollTopButton.style.display = 'block';
+        scrollTopButton.style.opacity = '1';
+        scrollTopButton.style.pointerEvents = 'auto';
+    } else {
+        scrollTopButton.style.opacity = '0';
+        scrollTopButton.style.pointerEvents = 'none';
+    }
+});
