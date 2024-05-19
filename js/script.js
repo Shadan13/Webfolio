82f2de5d42b981e2c978420c6a34f178cd7c9c8e
Shadan Siddiqui <shadan.siddiqui@gmail.com>
2024-05-19 15:11:09 +0800

    Update files via upload

--- js/script.js
+++ js/script.js
@@ -22,8 +22,6 @@ function scrollToTop() {
     });
 }
 
-let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0)
-
 window.addEventListener('scroll', function () {
     var scrollTopButton = document.querySelector('.scroll-top');
     if (this.window.scrollY > 200) {
@@ -35,3 +33,31 @@ window.addEventListener('scroll', function () {
         scrollTopButton.style.pointerEvents = 'none';
     }
 });
+
+function switchMode() {
+    const button = document.querySelector('.dark-light');
+    const currentIcon = button.querySelector('i');
+    const currentStyleSheet = document.querySelector('link[rel=\'stylesheet\']');
+    const logoImg = document.querySelector('.nav-logo img');
+
+    // Toggle icon
+    if (currentIcon.classList.contains('bxs-sun')) {
+        currentIcon.classList.replace('bxs-sun', 'bxs-moon');
+    } else {
+        currentIcon.classList.replace('bxs-moon', 'bxs-sun');
+    }
+
+    // Toggle stylesheet
+    if (currentStyleSheet.href.endsWith('style-dark.css')) {
+        currentStyleSheet.href = 'css/style-light.css';
+    } else {
+        currentStyleSheet.href = 'css/style-dark.css';
+    }
+
+    // Toggle logo image
+    if (logoImg.src.endsWith('logo-dark.png')) {
+        logoImg.src = 'images/logo-light.png';
+    } else {
+        logoImg.src = 'images/logo-dark.png';
+    }
+}
